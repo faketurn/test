8de66dd802b0c9bbbd7719c24e8f0881c83a45b8
faketurn <faketurn@gmail.com>
2012-05-22 22:26:12 +0900

    ついでに表の部分もforeachにしたった。

--- Seisekihyou.cs
+++ Seisekihyou.cs
@@ -14,8 +14,8 @@ namespace Seisekihyou
         public int rika;
         public int syakai;
 
-        public int goukei;
-        public decimal heikin;
+        // public int goukei;
+        // public decimal heikin;
         public int juni;
 
         // デフォルトコンストラクタ
@@ -28,8 +28,8 @@ namespace Seisekihyou
             this.rika = 0;
             this.syakai = 0;
 
-            goukei = 0;
-            heikin = 0;
+            // goukei = 0;
+            // heikin = 0;
             juni = 1;
         }
 
@@ -43,8 +43,8 @@ namespace Seisekihyou
             this.rika = rika;
             this.syakai = syakai;
 
-            goukei = 0;
-            heikin = 0;
+            // goukei = 0;
+            // heikin = 0;
             juni = 1;
         }
 
@@ -65,26 +65,8 @@ namespace Seisekihyou
         static void Main()
         {
             #region // 生徒が増えたらSeitoクラスで作成し、リストgakkyuuに追加する //
+
             List<Seito> gakkyuu = new List<Seito>();
-
-            /*
-            string namae;
-            string kokugo;
-            int kokugoi = 0;
-            int suugaku;
-            int eigo;
-            int rika;
-            int syakai;
-
-            Console.WriteLine("名前を入力してください");
-            namae = Console.ReadLine();
-            Console.WriteLine("国語の点数を入力してください");
-            kokugo = Console.ReadLine();
-            kokugoi = int.Parse(kokugo);
-
-            gakkyuu.Add(new Seito(namae, kokugoi, 1, 1, 1, 1));
-            */
-
             gakkyuu.Add(new Seito("ファナ　", 50, 43, 80, 70, 61));
             gakkyuu.Add(new Seito("アルシエ", 80, 60, 33, 70, 35));
             gakkyuu.Add(new Seito("ケスト　", 35, 59, 68, 65, 51));
@@ -94,6 +76,7 @@ namespace Seisekihyou
 
             #endregion
             #region// ヘッダー部分 //
+
             Console.WriteLine("{0,-8}{1,6}{2,4}{3,4}{4,4}{5,4}{6,4}{7,5}{8,4}", "名前", "国語", "数学", "英語", "理科", "社会", "合計点", "平均点", "順位");
 
             // 生徒個人の成績合計（横軸）
@@ -101,7 +84,6 @@ namespace Seisekihyou
 
             // 順位をつけるため配列gakkyuuの合計点を求めておく。
             // 同時に総合計点を求めるためsoukeiに合計点を足していく
-            // foreach使うってこういうことか？
             foreach (Seito kei in gakkyuu)
             {
                 soukei += kei.Goukei();
@@ -117,28 +99,25 @@ namespace Seisekihyou
                 }
             }
             #endregion
-
             #region // 配列gakkyuuの表示部分 //
-            for (int i = 0; i < gakkyuu.Count; i++)
-            {
-                Console.Write("{0,-8}", gakkyuu[i].namae);
 
-                // 科目ごとの点数を表示
-                Console.Write("{0,6}", gakkyuu[i].kokugo);
-                Console.Write("{0,6}", gakkyuu[i].suugaku);
-                Console.Write("{0,6}", gakkyuu[i].eigo);
-                Console.Write("{0,6}", gakkyuu[i].rika);
-                Console.Write("{0,6}", gakkyuu[i].syakai);
+            foreach (Seito item in gakkyuu)
+            {
+                Console.Write("{0,-8}", item.namae);
+                Console.Write("{0,6}", item.kokugo);
+                Console.Write("{0,6}", item.suugaku);
+                Console.Write("{0,6}", item.eigo);
+                Console.Write("{0,6}", item.rika);
+                Console.Write("{0,6}", item.syakai);
 
                 // すでに合計点は求められているのでメソッドではなく、変数の表示をする
-                Console.Write("{0,7}", gakkyuu[i].Goukei());
-
-                Console.Write("   {0,0:#.00}", gakkyuu[i].Heikin());
-
-                Console.Write("{0,4}位\n", gakkyuu[i].juni);
+                Console.Write("{0,7}", item.Goukei());
+                Console.Write("   {0,0:#.00}", item.Heikin());
+                Console.Write("{0,4}位\n", item.juni);
             }
             #endregion
             #region // 生徒全員の成績合計（縦軸）//
+
             Console.Write("{0,-10}", "合計");
 
             int kokugokei = 0;
